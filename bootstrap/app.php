2cc366bf202f6aa5e9b12b6e6b3950fda885c76a
fazha-zzz <fazha.mtegar22@gmail.com>
2026-04-09 08:16:00 +0700

    update payment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,6 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -13,6 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // --- TAMBAHKAN BAGIAN INI ---
+        $middleware->validateCsrfTokens(except: [
+            'api/midtrans/callback', 
+        ]);
+        // ----------------------------
+
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
             'isAdmin' => \App\Http\Middleware\IsAdmin::class,
@@ -22,9 +27,6 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withSchedule(function (Schedule $schedule) {
-        // Jalan setiap tanggal 1 jam 00:00
-        // $schedule->command('pembayaran:generate')->monthlyOn(1, '00:00');
         $schedule->command('pembayaran:generate')->everyMinute();
-
     })
     ->create();
